8b403ced9d981ff60081caf88728072288b6c04a
Judy Derrick <jgd6426@rit.edu>
2023-04-13 23:32:09 -0400

    tag demo

---
+++ Project2/Assets/Scripts/AgentManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AgentManager : MonoBehaviour
+{
+    public static AgentManager Instance;
+
+    [HideInInspector]
+    public List<TagPlayer> tagPlayers = new List<TagPlayer>();
+
+    [HideInInspector]
+    public Vector2 maxPosition = Vector2.one;
+    [HideInInspector]
+    public Vector2 minPosition = -Vector2.one;
+
+    public float edgePadding = 1f;
+
+    public TagPlayer tagPlayerPrefab;
+    public int numTagPlayers = 10;
+
+    public int countdownTime = 5;
+
+    [HideInInspector]
+    public TagPlayer currentItPlayer;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        Camera cam = Camera.main;
+
+        if (cam != null)
+        {
+            Vector3 camPosition = cam.transform.position;
+            float halfHeight = cam.orthographicSize;
+            float halfWidth = halfHeight * cam.aspect;
+
+            maxPosition.x = camPosition.x + halfWidth - edgePadding;
+            maxPosition.y = camPosition.y + halfHeight - edgePadding;
+
+            minPosition.x = camPosition.x - halfWidth + edgePadding;
+            minPosition.y = camPosition.y - halfHeight + edgePadding;
+        }
+
+        for (int i = 0; i < numTagPlayers; i++)
+        {
+            tagPlayers.Add(Spawn(tagPlayerPrefab));
+        }
+
+        tagPlayers[0].Tag();
+    }
+
+    private T Spawn<T>(T prefabToSpawn) where T : Agent
+    {
+        float xPos = Random.Range(minPosition.x, maxPosition.x);
+        float yPos = Random.Range(minPosition.y, maxPosition.y);
+
+        return Instantiate(prefabToSpawn, new Vector3(xPos, yPos), Quaternion.identity);
+    }
+
+    public TagPlayer GetClosestTagPlayer(TagPlayer sourcePlayer)
+    {
+        float minDistance = float.MaxValue;
+        TagPlayer closestPlayer = null;
+
+        foreach (TagPlayer other in tagPlayers)
+        {
+            float sqrDistance = Vector3.SqrMagnitude(sourcePlayer.physicsObject.Position - other.physicsObject.Position);
+
+            if (sqrDistance < float.Epsilon)
+            {
+                // this is the sourcePlayer
+                continue;
+            }
+
+            if (sqrDistance < minDistance)
+            {
+                closestPlayer = other;
+                minDistance = sqrDistance;
+            }
+        }
+
+        return closestPlayer;
+    }
+}
---
+++ Project2/Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    private Vector3 velocity;
+    private Vector3 acceleration;
+    private Vector3 direction;
+
+    public float mass = 2f;
+
+    public bool bounceOffWalls = false;
+
+    // public bool useGravity = false;
+    
+    public bool useFriction = false;
+    
+    public float frictionCoeff = 0.2f;
+
+    // private Vector3 cameraSize;
+
+    public Vector3 Velocity => velocity;
+    public Vector3 Direction => direction;
+    public Vector3 Position => transform.position;
+    // public Vector3 CameraSize => cameraSize;
+
+    public float radius = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cameraSize.y = Camera.main.orthographicSize;
+        //cameraSize.x = cameraSize.y * Camera.main.aspect;
+
+        direction = Random.insideUnitCircle.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (useGravity)
+        {
+            ApplyGravity(Physics.gravity);
+        }*/
+
+        if (useFriction)
+        {
+            ApplyFriction(frictionCoeff);
+        }
+
+        // Calculate a new velocity based on the current acceleration of this object
+        velocity += acceleration * Time.deltaTime;
+
+        // Calculate the new position based on the velocity for this frame
+        transform.position += velocity * Time.deltaTime;
+
+        if (velocity.sqrMagnitude > Mathf.Epsilon)
+        {
+            // Store the direction that the object is moving in
+            direction = velocity.normalized;
+        }
+
+        // Zero out the acceleration for the next frame
+        acceleration = Vector3.zero;
+
+        transform.rotation = Quaternion.LookRotation(Vector3.back, direction);
+
+        if (bounceOffWalls)
+        {
+            BounceOffWalls();
+        }
+    }
+
+    // Applies a force to this object following Newton's second law of motion
+    public void ApplyForce(Vector3 force)
+    {
+        acceleration += force / mass;
+    }
+    
+    // Applies a friction force to this object
+    private void ApplyFriction(float coeff)
+    {
+        Vector3 friction = velocity * -1;
+        friction.Normalize();
+        friction = friction * coeff;
+
+        ApplyForce(friction);
+    }
+
+/*    // Applies a gravitational force that acts on this object
+    private void ApplyGravity(Vector3 gravityForce)
+    {
+        acceleration += gravityForce;
+    }*/
+
+    private void BounceOffWalls()
+    {
+        // If we're off screen, and we're still moving off screen, change the direction we're moving in
+        if (transform.position.x > AgentManager.Instance.maxPosition.x && velocity.x > 0)
+        {
+            velocity.x *= -1f;
+            
+        }
+
+        if (transform.position.x < -AgentManager.Instance.minPosition.x && velocity.x < 0)
+        {
+            velocity.x *= -1f;
+        }
+
+        if (transform.position.y > AgentManager.Instance.maxPosition.y && velocity.y > 0)
+        {
+            velocity.y *= -1f;
+        }
+
+        if (transform.position.y < -AgentManager.Instance.minPosition.y && velocity.y < 0)
+        {
+            velocity.y *= -1f;
+        }
+    }
+}
